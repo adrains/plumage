cac740181b4d7784494d5f0978e3a58397d7c24a
Adam Rains <adam.d.rains@gmail.com>
2020-05-19 15:30:14 +1000

    Updated spectral resolution fitting code to fit for instrument resolving power (constant spectral resolution) rather than resolution (constant resolving power).

--- optimise_synth_fit_resolution.py
+++ optimise_synth_fit_resolution.py
@@ -1,13 +1,5 @@
 """
-Regions:
-
-Blue:
- - 3700-3900
- - 5000-5200
-
-Red:
- - 5600-5800
- - 6600-6800
+Script to fit for constant-wavelength resolution
 """
 import numpy as np
 import plumage.utils as utils
@@ -16,6 +8,9 @@ import astropy.constants as const
 from scipy.optimize import least_squares
 from scipy.interpolate import InterpolatedUnivariateSpline as ius
 
+# -----------------------------------------------------------------------------
+# Fitting Functions
+# -----------------------------------------------------------------------------
 def compute_resid(
     res, 
     params,
@@ -29,42 +24,42 @@ def compute_resid(
     wl_min,
     wl_max,
     wl_per_px,
+    inst_res_pow,
     ):
+    """Calculates the residual for a single fit
     """
-    """
-
     wave_synth, spec_synth = synth.get_idl_spectrum(
         idl, 
         params[0], 
         params[1], 
         params[2], 
-        wl_min, 
-        wl_max, 
-        res[0], 
+        wave_sci[0],                # wave_min
+        wave_sci[-1]+wl_per_px*0.5, # wave_max, *0.5 to not add two px
+        ipres=inst_res_pow,
+        resolution=res[0], 
         norm="abs",
         do_resample=True, 
-        wl_per_pixel=wl_per_px,
+        wl_per_px=wl_per_px,
+        rv_bcor=(rv-bcor),
         )
 
-    # The grid we put our new synthetic spectrum on should be put in the same
-    # RV frame as the science spectrum
-    wave_rv_scale = 1 - (rv - bcor)/(const.c.si.value/1000)
-    ref_spec_interp = ius(wave_synth, spec_synth)
-
-    wave_synth = wave_sci * wave_rv_scale
-    spec_synth = ref_spec_interp(wave_synth)
+    if len(spec_sci) != len(spec_synth):
+        import pdb
+        pdb.set_trace()
 
     # Normalise spectra by median before fit
     e_spec_sci /= np.nanmedian(spec_sci)
     spec_sci /= np.nanmedian(spec_sci)
     spec_synth /= np.nanmedian(spec_synth)
 
-    # Calculate the residual
-    resid_vect = (spec_sci[~bad_px_mask] 
-                  - spec_synth[~bad_px_mask]) / e_spec_sci[~bad_px_mask]
+    # Do bad pixel masking by setting sigma to infinity, and the flux to one
+    spec_sci_m = spec_sci.copy()
+    spec_sci_m[bad_px_mask] = 1
+    e_spec_sci_m = e_spec_sci.copy()
+    e_spec_sci_m[bad_px_mask] = np.inf
 
-    if not np.isfinite(np.sum(resid_vect)):
-        resid_vect = np.ones_like(resid_vect) * 1E30
+    # Calculate the residual
+    resid_vect = (spec_sci_m - spec_synth) / e_spec_sci_m
 
     return resid_vect
 
@@ -81,18 +76,18 @@ def fit_for_res(
     wl_min,
     wl_max,
     wl_per_px,
+    inst_res_pow,
     res,):
-    """
+    """Fit for constant-wavelength resolution for a single spectrum.
     """
     # Setup fit settings
     args = (params, wave, spec_sci, e_spec_sci, bad_px_mask, rv, bcor , idl,
-            wl_min, wl_max, wl_per_px)
-    bounds = ((1000,), (100000,))
-    scale = (1,)
+            wl_min, wl_max, wl_per_px, inst_res_pow)
+    bounds = ((0.01,), (5,))
+    scale = (0.01,)
     step = (1,)
-    
-    # Make sure initial teff guess isn't out of bounds, assign default if so
-    #res = res
+
+    # Initialise resolution guess
     res = np.array([res])
 
     # Do fit
@@ -101,8 +96,8 @@ def fit_for_res(
         res, 
         jac="3-point",
         bounds=bounds,
-        x_scale=scale,
-        diff_step=step,
+        #x_scale=scale,
+        #diff_step=step,
         args=args, 
     )
 
@@ -117,24 +112,25 @@ def fit_for_many_res(
     observations, 
     wl_mins,
     wl_maxs,
+    wl_per_px,
     star_is,
+    inst_res_pow,
     res,):
-    """
+    """Fit for constant-wavelength resolution for many spectra
     """
     res_fits_all = []
 
     for star_i, params in zip(star_is, params_all):
 
-        print("\n{}".format(observations.iloc[star_i]["id"]))
+        print("\nGaia DR2 {}".format(observations.iloc[star_i]["id"]))
         res_fits = []
 
         for wl_min, wl_max in zip(wl_mins, wl_maxs):
 
-            wl_mask = np.logical_and(spectra[star_i,0] > wl_min, 
-                                    spectra[star_i,0] < wl_max)
+            wl_mask = np.logical_and(spectra[star_i,0] >= wl_min, 
+                                    spectra[star_i,0] <= wl_max)
 
-            wl_per_px = (wl_max - wl_min) / len(spectra[star_i, 0][wl_mask])
-
+            # Do fit
             opt_res = fit_for_res(
                 idl,
                 params,
@@ -147,7 +143,8 @@ def fit_for_many_res(
                 wl_min,
                 wl_max,
                 wl_per_px,
-                res,)
+                inst_res_pow,
+                res)
 
             # Calculate uncertainties
             jac = opt_res["jac"]
@@ -155,7 +152,7 @@ def fit_for_many_res(
             cov = np.linalg.inv(jac.T.dot(jac))
             std = np.sqrt(np.diagonal(cov)) * np.nanvar(residuals)
 
-            print("{:0.0f}-{:0.0f} A --> R~{:0.0f}+/-{:0.0f}".format(
+            print("{:0.0f}-{:0.0f} A --> R~{:0.3f} +/- {:0.3f} A".format(
                 wl_min, wl_max, float(opt_res["x"]), float(std)))
 
             res_fits.append([float(opt_res["x"]), float(std)])
@@ -169,6 +166,9 @@ def fit_for_many_res(
 
     return mn, std
 
+# -----------------------------------------------------------------------------
+# Script
+# -----------------------------------------------------------------------------
 # Import
 label = "std"
 spec_path = "spectra"
@@ -197,12 +197,18 @@ params_all = [
     [5347, 4.53, -0.55],
     [4505, 4.76, 0.12]]
 
-print("-"*40, 'Fitting Blue')
+print("-"*40, '\nFitting Blue')
 
 # Do blue fit
+res_init_b = (5700 - (5700-3500)/2) / 3000
 wl_step = 400
 wl_mins_b = np.arange(3600, 5400, wl_step)
 wl_maxs_b = wl_mins_b + wl_step
+wl_per_px_b = 0.77
+
+# Add in maxes/mins for entire spectrum
+wl_mins_b = np.concatenate((wl_mins_b, [3600]))
+wl_maxs_b = np.concatenate((wl_maxs_b, [5700]))
 
 blue_res, e_blue_res = fit_for_many_res(
     idl, 
@@ -212,18 +218,26 @@ blue_res, e_blue_res = fit_for_many_res(
     observations, 
     wl_mins_b,
     wl_maxs_b,
+    wl_per_px_b,
     row_is,
-    3000,)
+    3000,
+    res_init_b)
 
 w_res_b = np.average(blue_res, axis=0, weights=e_blue_res**-2)
 w_e_res_b = (np.sum(e_blue_res**-2,axis=0)**-1)**0.5 
 
-print("-"*40, 'Fitting Red')
+print("-"*40, '\nFitting Red')
 
 # Do red fit
+res_init_r = (7000 - (7000-5400)/2) / 7000
 wl_step = 400
 wl_mins_r = np.arange(5400, 6850, wl_step)
 wl_maxs_r = wl_mins_r + wl_step
+wl_per_px_r = 0.44
+
+# Add in maxes/mins for entire spectrum
+wl_mins_r = np.concatenate((wl_mins_r, [5400]))
+wl_maxs_r = np.concatenate((wl_maxs_r, [7000]))
 
 red_res, e_red_res = fit_for_many_res(
     idl, 
@@ -233,8 +247,10 @@ red_res, e_red_res = fit_for_many_res(
     observations, 
     wl_mins_r,
     wl_maxs_r,
+    wl_per_px_r,
     row_is,
-    7000,)
+    7000,
+    res_init_r)
 
 w_res_r = np.average(red_res, axis=0, weights=e_red_res**-2)
 w_e_res_r = (np.sum(e_red_res**-2,axis=0)**-1)**0.5 
