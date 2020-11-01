Updates to transit fitting and plotting, fixed bug with masking, adding extra terms for flattening.

lightcurve_fit.py
@@ -40,14 +40,22 @@ comb_info = info.join(observations, on="source_id", lsuffix="", rsuffix="_2", ho
 # Intitialise limb darkening coefficient columns
 ldc_cols = ["ldc_a1", "ldc_a2", "ldc_a3", "ldc_a4"]
 
+# Set a default period uncertainty if we don't have one
 mean_e_period = 10 / 60 / 24
 
-# Min window size
+# Amount of lightcurve to fit to inn units of transit duration
+n_trans_dur = 1.2
+
+# Inflate the error on our fitted stellar radii
+e_radius_mult = 10
+
+# Min window size for flattening in days
 t_min = 8/24
 
 # Plotting settings
 binsize = 10
 bin_lightcurve = True
+break_tolerance = 10
 
 # -----------------------------------------------------------------------------
 # Do fitting
@@ -84,8 +92,14 @@ for toi_i, (toi, toi_row) in enumerate(comb_info.iterrows()):
         print("Skipping: No transit duration\n")
         all_fits[toi] = None
         continue
+    #elif toi != 177.01:#270.02: #406.01:#
+        #continue
     else:
-        lightcurve = light_curves[tic].remove_nans()
+        # Bin lightcurve
+        if bin_lightcurve:
+            lightcurve = light_curves[tic].remove_nans().bin(binsize=binsize)
+        else:
+            lightcurve = light_curves[tic].remove_nans()
 
     # Calculate semi-major axis and scaled semimajor axis
     if np.isnan(toi_row["Period error"]):
@@ -99,9 +113,9 @@ for toi_i, (toi, toi_row) in enumerate(comb_info.iterrows()):
         toi_row["Period (days)"],
         e_period,
         toi_row["radius"],
-        toi_row["e_radius"],
+        toi_row["e_radius"]*e_radius_mult,
         )
-
+    
     # Get mask for all transits with this system
     mask = transit.make_transit_mask_all_periods(
         lightcurve, 
@@ -120,7 +134,11 @@ for toi_i, (toi, toi_row) in enumerate(comb_info.iterrows()):
             e_sma_rstar,
             mask,
             ld_model="nonlinear",
-            t_min=t_min,)
+            t_min=t_min,
+            verbose=True,
+            n_trans_dur=n_trans_dur,
+            binsize=binsize,
+            bin_lightcurve=bin_lightcurve,)
 
     except transit.BatmanError:
         print("\nBatman failure, skipping\n")
@@ -134,10 +152,14 @@ for toi_i, (toi, toi_row) in enumerate(comb_info.iterrows()):
         continue
 
     # Calclate the planet radii
-    radius = (opt_res["x"][0] * toi_row["radius"]
-              * const.R_sun.si.value / const.R_earth.si.value)
-    e_radius = (opt_res["std"][0] * toi_row["radius"]
-              * const.R_sun.si.value / const.R_earth.si.value)
+    rp_rstar_fit = opt_res["x"][0]
+    e_rp_rstar_fit = opt_res["std"][0]
+
+    r_e = const.R_sun.si.value / const.R_earth.si.value
+    
+    r_p = rp_rstar_fit * toi_row["radius"] * r_e
+    e_r_p = r_p * np.sqrt((e_radius_mult*toi_row["e_radius"]/toi_row["radius"])**2
+                           + (e_rp_rstar_fit/rp_rstar_fit)**2) * r_e
 
     # Record fit dictionaries
     all_fits[toi] = opt_res
@@ -145,7 +167,7 @@ for toi_i, (toi, toi_row) in enumerate(comb_info.iterrows()):
     # Save calculated params + uncertainties
     result_df.loc[toi][["sma", "e_sma"]] = [sma, e_sma]
     result_df.loc[toi][["sma_rstar_fit", "e_sma_rstar_fit"]] = [sma_rstar, e_sma_rstar]
-    result_df.loc[toi][["rp_fit", "e_rp_fit"]] = [radius, e_radius]
+    result_df.loc[toi][["rp_fit", "e_rp_fit"]] = [r_p, e_r_p]
 
     # Save fitted parameters
     param_cols = ["rp_rstar_fit", "sma_rstar_fit", "inclination_fit"]
@@ -188,7 +210,13 @@ pplt.plot_all_lightcurve_fits(
     tic_info,
     observations,
     binsize=binsize,
-    bin_lightcurve=bin_lightcurve,)
+    bin_lightcurve=bin_lightcurve,
+    break_tolerance=break_tolerance,)
+
+pplt.merge_spectra_pdfs(
+    "plots/lc_diagnostics/*.pdf", 
+    "plots/lc_diagnostics.pdf",) 
 
 # Save results
+toi_info.index.set_names(["TOI"], inplace=True)
 utils.save_fits_table("TRANSIT_FITS", toi_info, "tess")

plumage/plotting.py
@@ -2081,7 +2081,7 @@ def plot_label_comparison(
 # -----------------------------------------------------------------------------
 # Planet plots
 # ----------------------------------------------------------------------------- 
-def plot_planet_radii_hist(lc_results, bin_width=0.4):
+def plot_planet_radii_hist(lc_results, bin_width=0.4, min_rp=0.1,):
     """Plots a histogram of stellar radii.
 
     Parameters
@@ -2095,8 +2095,9 @@ def plot_planet_radii_hist(lc_results, bin_width=0.4):
     plt.close("all")
     fig, axis = plt.subplots()
 
+    min_rp = np.min(lc_results["rp_fit"][lc_results["rp_fit"] > min_rp])
     max_rp = np.max(lc_results["rp_fit"])
-    bin_edges = np.arange(0, max_rp+bin_width, bin_width)
+    bin_edges = np.arange(min_rp, max_rp+bin_width, bin_width)
 
     axis.hist(lc_results["rp_fit"], bins=bin_edges)
 
@@ -2211,15 +2212,24 @@ def plot_lightcurve_fit(lightcurve, folded_lc, flat_lc_trend, bm_params, bm_mode
     ax_lc_folded_transit.set_xlim((-2*trans_dur/period, 2*trans_dur/period))
 
     # Figure out our y limits
-    lim = np.nanstd(folded_lc.flux)*5.5 + np.nanmean(folded_lc.flux_err)
+    transit_mask = np.logical_and(
+        folded_lc.time < trans_dur/period/2,
+        folded_lc.time > -trans_dur/period/2)
+        
+    y_min = 2*(1-np.mean(folded_lc.flux[transit_mask]))
 
-    ax_lc_unfolded.set_ylim((1-lim, 1+lim))
-    ax_lc_folded_all.set_ylim((1-lim, 1+lim))
-    ax_lc_folded_transit.set_ylim((1-lim, 1+lim))
+    std_lim = np.nanstd(folded_lc.flux)*5.5 + np.nanmean(folded_lc.flux_err)
+
+    # Use whichever is lower
+    y_lim = y_min if y_min > std_lim else std_lim
+
+    ax_lc_unfolded.set_ylim((1-std_lim, 1+std_lim))
+    ax_lc_folded_all.set_ylim((1-y_lim, 1+std_lim))
+    ax_lc_folded_transit.set_ylim((1-y_lim, 1+std_lim))
 
 
 def plot_all_lightcurve_fits(light_curves, toi_info, tess_info, observations,
-        binsize=4, bin_lightcurve=False,):
+        binsize=4, bin_lightcurve=False, break_tolerance=None):
     """Plot PDFs of all TESS light curve fits.
 
     Parameters
@@ -2299,7 +2309,7 @@ def plot_all_lightcurve_fits(light_curves, toi_info, tess_info, observations,
             window_length=int(toi_row["window_length"]),
             return_trend=True,
             niters=int(toi_row["niters_flat"]),
-            break_tolerance=None,
+            break_tolerance=break_tolerance,
             mask=mask)
 
         # Phase fold the light curve

plumage/transits.py
@@ -348,7 +348,12 @@ def fit_light_curve(
     flatten_frac=0.1, 
     outlier_sig=6,
     niters_flat=5,
-    t_min=1/24,):
+    t_min=1/24,
+    verbose=True, 
+    n_trans_dur=2,
+    binsize=2,
+    bin_lightcurve=False,
+    break_tolerance=100,):
     """Perform least squares fitting on the provided light curve to determine
     Rp/R*, a/R*, and inclination.
 
@@ -395,15 +400,15 @@ def fit_light_curve(
     # Before cleaning however, we should mask out the transit signal itself
     #clean_lc = light_curve.remove_outliers(sigma=outlier_sig)
 
-    # Get window size for smoothing
+    # Get window size for smoothing (note mask inversion)
     window_length = determine_window_size(light_curve, t0, period, trans_dur, 
-        mask, t_min)
+        ~mask, t_min,)
 
     clean_lc, flat_lc_trend = light_curve.flatten(
         window_length=window_length,
         return_trend=True,
         niters=niters_flat,
-        break_tolerance=None,
+        break_tolerance=100,
         mask=mask)
 
     # Phase fold the light curve
@@ -412,11 +417,11 @@ def fit_light_curve(
     # Initialise transit params. Ftting for: [rp, semi-major axis, inclination]
     # and assuming a circular orbit, so eccentricity=1, & longitude periastron
     # isn't relevant
-    init_params = np.array([0.1, 10, 90,])
+    init_params = np.array([0.05, sma_rstar, 90,])
     bounds = ((0, 0, 60,), (1, 10000, 120,))
 
     args = (folded_lc, t0, period, ld_model, ld_coeff, trans_dur, sma_rstar,
-            e_sma_rstar)
+            e_sma_rstar, verbose, n_trans_dur,)
 
     #scale = (1, 1, 1)
     step = (0.1, 0.1, 0.1)
@@ -468,6 +473,7 @@ def fit_light_curve(
         time=folded_lc.time,)
 
     # Add extra info to fit dictionary
+    opt_res["folded_lc"] = folded_lc
     opt_res["window_length"] = window_length
     opt_res["niters_flat"] = niters_flat
     opt_res["flat_lc_trend"] = flat_lc_trend
@@ -507,7 +513,7 @@ def make_transit_mask_single_period(
     Returns
     -------
     mask: bool array
-    
+        True where transits occur
     """
     # Work in modulo time of the period
     mod_time = (light_curve.time - t0) % period
@@ -533,7 +539,7 @@ def make_transit_mask_all_periods(light_curve, toi_info, tic_id):
     """
     selected_tois = toi_info[toi_info["TIC"]==tic_id]
 
-    mask = np.full(len(light_curve.time), True)
+    mask = np.full(len(light_curve.time), False)
 
     for star_i, toi_row in selected_tois.iterrows():
         # Only contribute to mask if we have non-nan parameters
@@ -541,9 +547,9 @@ def make_transit_mask_all_periods(light_curve, toi_info, tic_id):
             print("Skipped TOI {} when making mask".format(toi_row.name))
             continue
 
-        mask = ~np.logical_and(
+        mask = np.logical_or(
             mask,
-            ~make_transit_mask_single_period(
+            make_transit_mask_single_period(
                 light_curve,
                 toi_row["Epoch (BJD)"]-BTJD_OFFSET,
                 toi_row["Period (days)"],
@@ -553,8 +559,11 @@ def make_transit_mask_all_periods(light_curve, toi_info, tic_id):
     return mask
 
 
-def determine_window_size(light_curve, t0, period, trans_dur, mask, t_min=1/24):
+def determine_window_size(light_curve, t0, period, trans_dur, mask, t_min=1/24,
+    show_diagnostic_plot=False,):
     """
+    mask: bool arrays
+        True where we should use.
     """
     # Get mask of transits
     #mask = ~make_transit_mask(light_curve, t0, period, trans_dur,)
@@ -570,8 +579,11 @@ def determine_window_size(light_curve, t0, period, trans_dur, mask, t_min=1/24):
     cadence = np.median(light_curve.time[1:] - light_curve.time[:-1])
 
     # Calculate window length
-    window_length = int(stellar_period / cadence)
+    window_length = int(stellar_period / cadence / 4)
 
+    if show_diagnostic_plot:
+        plt.plot(1/freq, power)
+    
     if window_length % 2 == 0:
         window_length += 1
 