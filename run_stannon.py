Updates to run_stannon script (TODO: generalise to either benchmarks or science targets), bug fix in error computation from label inference, generalising model spectra plotting, new LaTeX table for inferred labels.

run_stannon.py
@@ -7,7 +7,7 @@ import plumage.utils as utils
 import plumage.plotting as pplt
 import stannon.stannon as stannon
 import stannon.plotting as splt
-import matplotlib.pyplot as plt
+import stannon.tables as stab
 
 #------------------------------------------------------------------------------
 # Parameters and Setup
@@ -104,7 +104,7 @@ ivars_norm[bad_px_mask] = 0
 # Predict labels for TESS targets
 #------------------------------------------------------------------------------
 # Predict labels
-tess_labels_pred, tess_errs_all, tess_chi2_all = sm.infer_labels(
+tess_labels_pred, e_tess_labels_pred, tess_chi2_all = sm.infer_labels(
     fluxes_norm[:,sm.adopted_wl_mask],
     ivars_norm[:,sm.adopted_wl_mask])
 
@@ -115,8 +115,9 @@ tess_labels_pred -= systematic_vector
 
 # Create uncertainties vector. TODO: do this in quadrature with those output
 # from infer_labels
-uncertainties_vector = np.tile(adopted_label_uncertainties, np.sum(tess_mask))
-e_fit = uncertainties_vector.reshape([np.sum(tess_mask), n_labels])
+cross_val_sigma = np.tile(adopted_label_uncertainties, np.sum(tess_mask))
+cross_val_sigma = cross_val_sigma.reshape([np.sum(tess_mask), n_labels])
+e_tess_labels_pred = np.sqrt(e_tess_labels_pred**2 + cross_val_sigma**2)
 
 """
 # Plot a joint CMD of benchmarks and science targets
@@ -138,11 +139,38 @@ splt.plot_cannon_cmd(
 # Plot Kiel Diagram for results
 splt.plot_kiel_diagram(
     teffs=tess_labels_pred[:,0],
-    e_teffs=e_fit[:,0],
+    e_teffs=e_tess_labels_pred[:,0],
     loggs=tess_labels_pred[:,1],
-    e_loggs=e_fit[:,1],
+    e_loggs=e_tess_labels_pred[:,1],
     fehs=tess_labels_pred[:,2],
     label="science",)
+
+# Plot spectra comparison
+bp_rp_order = np.argsort(obs_join_tess[tess_mask]["Bp-Rp"].values)
+splt.plot_spectra_comparison(
+    sm=sm,
+    obs_join=obs_join_tess[tess_mask],
+    fluxes=fluxes_norm,
+    bad_px_masks=bad_px_mask,
+    labels_all=tess_labels_pred,
+    source_ids=obs_join_tess[tess_mask].index[bp_rp_order],
+    x_lims=(wl_min_model,wl_max_model),
+    fn_label="d",
+    data_label="tess",
+    star_name_col="TOI")
+
+caption = "TESS candidate planet host parameter fits (corrected for systematics)"
+
+# Make results table
+stab.make_table_parameter_fit_results(
+    obs_tab=obs_join_tess[tess_mask],
+    label_fits=tess_labels_pred,
+    e_label_fits=e_tess_labels_pred,
+    abundance_labels=abundance_labels,
+    break_row=61,
+    star_label=("TOI", "TOI"),
+    table_label="tess",
+    caption=caption,)
 
 #------------------------------------------------------------------------------
 # Planet-star correlations

stannon/plotting.py
@@ -620,11 +620,16 @@ def plot_theta_coefficients(
 def plot_spectra_comparison(
     sm,
     obs_join,
+    fluxes,
+    bad_px_masks,
+    labels_all,
     source_ids,
     y_offset=1.8,
     x_lims=(5400,7000),
     x_ticks=(200,100),
-    fn_label="",):
+    fn_label="",
+    data_label="",
+    star_name_col="simbad_name",):
     """Plot a set of observed spectra against their Cannon generated spectra
     equivalents.
     """
@@ -650,8 +655,8 @@ def plot_spectra_comparison(
         hatch=None)
 
     # Do bad px masking
-    masked_spectra = sm.training_data.copy()
-    masked_spectra[sm.bad_px_mask] = np.nan
+    masked_spectra = fluxes.copy()
+    masked_spectra[bad_px_masks] = np.nan
 
     # For every star in source_ids, plot blue and red spectra
     for star_i, source_id in enumerate(source_ids):
@@ -659,9 +664,9 @@ def plot_spectra_comparison(
         bm_i = int(np.argwhere(obs_join.index == source_id))
 
         # Generate a model spectrum (with nans for our excluded regions)
-        labels = sm.training_labels[bm_i]
+        labels = labels_all[bm_i]
 
-        spec_gen = np.full(sm.training_data.shape[1], np.nan)
+        spec_gen = np.full(fluxes.shape[1], np.nan)
         spec_gen[sm.adopted_wl_mask] = sm.generate_spectra(labels)
 
         # Plot observed spectrum
@@ -683,7 +688,7 @@ def plot_spectra_comparison(
         # Label spectrum
         star_txt = r"{}, $T_{{\rm eff}}={:0.0f}\,$K, [Fe/H]$ = ${:+.2f}"
         star_txt = star_txt.format(
-            obs_join.loc[source_id]["simbad_name"],
+            obs_join.loc[source_id][star_name_col],
             labels[0],
             labels[2])
 
@@ -711,5 +716,5 @@ def plot_spectra_comparison(
     ax.set_xlabel("Wavelength (A)")
     plt.tight_layout()
 
-    plt.savefig("paper/cannon_spectra_comp_{}.pdf".format(fn_label))
-    plt.savefig("paper/cannon_spectra_comp_{}.png".format(fn_label), dpi=200)
+    plt.savefig("paper/cannon_spectra_comp_{}_{}.pdf".format(data_label, fn_label))
+    plt.savefig("paper/cannon_spectra_comp_{}_{}.png".format(data_label, fn_label), dpi=200)

stannon/stan_utils.py
@@ -265,7 +265,7 @@ def infer_labels(theta, scatter, fluxes, ivars, lbl_mean, lbl_std, n_labels=3):
                 * ivar / (1 + ivar * scatter**2))
         chi2_all[star_i] = sum(chi2)
         labels_all[star_i,:] = labels * lbl_std + lbl_mean
-        errs_all[star_i,:] = np.sqrt(cov.diagonal()) * lbl_std + lbl_mean
+        errs_all[star_i,:] = np.sqrt(cov.diagonal()) * lbl_std
 
     return labels_all, errs_all, chi2_all
 

stannon/stannon.py
@@ -537,8 +537,7 @@ class Stannon(object):
                     * ivar / (1 + ivar * self.s2**2))
             chi2_all[star_i] = sum(chi2)
             labels_all[star_i,:] = labels * self.std_labels + self.mean_labels
-            errs_all[star_i,:] = (np.sqrt(cov.diagonal()) * self.std_labels
-                                  + self.mean_labels)
+            errs_all[star_i,:] = (np.sqrt(cov.diagonal()) * self.std_labels)
 
         return labels_all, errs_all, chi2_all
 

train_stannon.py
@@ -261,6 +261,9 @@ representative_stars_source_ids = [
 splt.plot_spectra_comparison(
     sm=sm,
     obs_join=obs_join[std_mask],
+    fluxes=sm.training_data,
+    bad_px_masks=sm.bad_px_mask,
+    labels_all=labels_pred,
     source_ids=representative_stars_source_ids,
     x_lims=(wl_min_model,5400),
     fn_label="b",)
@@ -269,6 +272,9 @@ splt.plot_spectra_comparison(
 splt.plot_spectra_comparison(
     sm=sm,
     obs_join=obs_join[std_mask],
+    fluxes=sm.training_data,
+    bad_px_masks=sm.bad_px_mask,
+    labels_all=labels_pred,
     source_ids=representative_stars_source_ids,
     x_lims=(5400,wl_max_model),
     fn_label="r",)
@@ -278,9 +284,13 @@ bp_rp_order = np.argsort(obs_join[std_mask]["Bp-Rp"])
 splt.plot_spectra_comparison(
     sm=sm,
     obs_join=obs_join[std_mask],
+    fluxes=sm.training_data,
+    bad_px_masks=sm.bad_px_mask,
+    labels_all=labels_pred,
     source_ids=obs_join[std_mask].index[bp_rp_order],
     x_lims=(wl_min_model,wl_max_model),
-    fn_label="d",)
+    fn_label="d",
+    data_label="benchmark",)
 
 # Tabulate our adopted benchmark parameters
 stab.make_table_benchmark_overview(