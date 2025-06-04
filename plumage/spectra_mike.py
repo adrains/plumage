67c02ee1e5063a43aa585aef5daf3725d5040908
Adam D Rains <adam.d.rains@gmail.com>
2025-06-04 16:31:26 -0400

    MIKE spectra are now loaded in, co-added, paired for B/R, missing orders replaced, and diagnostics for each exposure plotted.

--- plumage/spectra_mike.py
+++ plumage/spectra_mike.py
@@ -3,14 +3,21 @@
 TODO: there should be a dedicated spectra module, with e.g.:
     plumage.spectra.wifes and plumage.spectra.mike
 """
+import os
+import glob
 import numpy as np
+import pandas as pd
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 from astropy.io import fits
 import warnings
+from tqdm import tqdm
 import astropy.constants as const
 from astropy import units as u
 from astropy.time import Time
 from scipy.interpolate import interp1d
 from astropy.coordinates import SkyCoord, EarthLocation
+import matplotlib.ticker as plticker
 
 HEADERS_TO_EXTRACT = ["SITENAME", "SITEALT", "SITELAT", "SITELONG",
         "TELESCOP", "OBSERVER", "DATE-OBS", "UT-DATE", "UT-START", "LC-START",
@@ -58,6 +65,12 @@ def interpolate_wavelength_scale(
         # Create interpolator for only non-nan px
         is_finite = np.isfinite(spec_old[order_i])
 
+        # If we have an order that is entirely NaN, skip interpolating it
+        if np.sum(is_finite) == 0:
+            spec_new[order_i] = spec_old[order_i]
+            continue
+
+        # Otherwise proceed with the interpolation
         interpolate_spec = interp1d(
             x=wave_old[order_i][is_finite],
             y=spec_old[order_i][is_finite],
@@ -126,6 +139,100 @@ def compute_barycentric_correction(
         bcors.append(barycorr.to(u.km/u.s).value)
 
     return bcors
+
+
+def unify_wavelength_scales(exp_dict,):
+    """Unifies the dimensions of the wavelength scales for exposures of the
+    same targets taken in sequence in the event one or more orders did not
+    extract properly. Missing stellar orders are populated with NaNs, whereas
+    the wavelength scale and any calibration fluxes are copied from the
+    reference exposure which has all orders extracted.
+
+    exp_dict is updated in place.
+
+    Parameters
+    ----------
+    all_exp_dict: dict
+        Dictionary representation of MIKE fits files, with keys for relevant
+        header keywords, the wavelength scale, and each of the seven (sky, obj,
+        sigma, snr, lamp, flat, obj / flat) data-cubes. Each key corresponds to
+        a list of length the number of exposures.
+    """
+    # These are [n_order, n_px] arrays we need to check the shapes of
+    spec_keywords = ["wave", "spec_sky", "spec_star", "sigma_star", "snr_star", 
+                     "spec_lamp", "spec_flat", "spec_star_norm"]
+
+    # These are the arrays that, if missing, we adopt from our reference exp
+    spec_keywords_to_dup = ["wave", "spec_sky", "spec_lamp", "spec_flat",]
+
+    # Grab the number of exposures for reference
+    n_exp = len(exp_dict["wave"])
+
+    # Check all wavelength scales
+    wl_shapes = [exp_dict["wave"][exp_i].shape for exp_i in range(n_exp)]
+
+    # If we only have one shape, all wavelength scales are the same, return
+    if len(set(wl_shapes)) == 1:
+        print("\tOrders all equal, continuing")
+        return
+
+    # Determine the reference wavelength scale to adopt based on which has the
+    # most px. 
+    total_px = np.prod(wl_shapes, axis=1)
+    ref_exp_i = np.argmax(total_px)
+    wave_ref = exp_dict["wave"][ref_exp_i].copy()
+
+    # Compute (rounded) order wavelengths for this selected exposure
+    order_means_ref = (np.round(np.mean(wave_ref/5, axis=1))*5).astype(int)
+
+    # Loop over all exposures and determine which order/s are missing
+    for exp_i in range(n_exp):
+        # Skip the reference exposure
+        if exp_i == ref_exp_i:
+            continue
+        
+        print("\tCorrecting for missing order on {} {}, {} --> {}".format(
+            exp_dict["DATE-OBS"][exp_i],
+            exp_dict["OBJECT"][exp_i],
+            wl_shapes[exp_i],
+            wave_ref.shape,
+            ),)
+
+        wave_ith = exp_dict["wave"][exp_i]
+
+        # Compute (rounded) order wavelength scales for this exposure
+        order_means = (np.round(np.mean(wave_ith/5, axis=1))*5).astype(int)
+        
+        # Determine which orders are missing
+        matched_orders = np.isin(order_means_ref, order_means)
+        
+        # For all [n_order, n_px] arrays for this exposure, adopt the values
+        # from this exposure where possible. For missing orders we either
+        # simply adopt the reference values (for the wavelength scale and
+        # calibration data), or add a dummy empty order full of NaNs (for the
+        # stellar spectrum).
+        for spec_key in spec_keywords:
+            # Grab the arrays for the reference and current exposures
+            array_ref = exp_dict[spec_key][ref_exp_i]
+            array_ith = exp_dict[spec_key][exp_i]
+
+            # Make a new array with the reference shape, and copy over the
+            # values from the overlapping orders. 
+            spec_array_new = np.full_like(wave_ref, np.nan)
+            spec_array_new[matched_orders,:] = array_ith
+
+            # For the wavelength scale and calibration data, duplicate the
+            # missing order/s rather than simply leaving them NaN as we do for
+            # the star.
+            if spec_key in spec_keywords_to_dup:
+                spec_array_new[~matched_orders,:] = \
+                    array_ref[~matched_orders,:]
+
+            # Save the new array back to the list
+            exp_dict[spec_key][exp_i] = spec_array_new
+
+    # All done, everything has been updated in place
+
 
 # -----------------------------------------------------------------------------
 # Reading fits files
@@ -279,7 +386,7 @@ def load_single_mike_fits(fn):
     return data_dict
 
 
-def load_and_combine_single_star(filenames,):
+def load_and_combine_single_star(filenames, plot_folder,):
     """Load in a list of fits files corresponding to reduced MIKE data of a
     *single* arm, and co-add the spectra.
 
@@ -287,6 +394,9 @@ def load_and_combine_single_star(filenames,):
     ----------
     filenames: str list
         List of filenames of reduced MIKE fits files to open.
+
+    plot_folder: str, default: ''
+        Base folder to save diagnostic reduction plots to.
 
     Returns
     -------
@@ -307,9 +417,33 @@ def load_and_combine_single_star(filenames,):
     all_exp_dict = {}
 
     # Read data from all exposures
-    for fn in filenames:
+    for fn_i, fn in enumerate(filenames):
         exp_dict = load_single_mike_fits(fn)
 
+        # Diagnostic Plot
+        plot_fn = "{}_{}_{}_{}.pdf".format(
+            exp_dict["UT-DATE"],
+            exp_dict["OBJECT"],
+            exp_dict["INSTRUME"].split("-")[-1],
+            fn_i)
+        
+        plot_title = "{}, {}, {}, SNR~{:0.0f}, # order = {}, exp {}/{}".format(
+            exp_dict["DATE-OBS"],
+            exp_dict["OBJECT"],
+            exp_dict["INSTRUME"].split("-")[-1],
+            np.nanmean(exp_dict["snr_star"]),
+            exp_dict["wave"].shape[0],
+            fn_i+1,
+            len(filenames),)
+
+        visualise_mike_spectra(
+            exp_dict, 
+            plot_folder,
+            plot_fn,
+            plot_title,
+            title_colour="r" if "Red" in plot_title else "b",)
+
+        # Save everything to one dictionary for all exposures
         for key in exp_dict.keys():
             if key in all_exp_dict.keys():
                 all_exp_dict[key].append(exp_dict[key])
@@ -328,6 +462,9 @@ def load_and_combine_single_star(filenames,):
         if len(set(all_exp_dict[header])) != 1:
             raise Exception(
                 "{} is not consistent for all exposures.".format(header))
+
+    # Ensure we have a consistent number of orders
+    unify_wavelength_scales(all_exp_dict)
 
     # -------------------------------------------------------------------------
     # Flat field normalisation
@@ -428,3 +565,175 @@ def load_and_combine_single_star(filenames,):
             obj_dict[header] = np.sum(all_exp_dict[header])
 
     return all_exp_dict, obj_dict
+
+
+def load_all_mike_fits(
+    spectra_folder="spectra/",
+    id_crossmatch_fn="data/mike_id_cross_match.tsv",
+    plot_folder="",):
+    """Load in all fits cubes containing 1D spectra to extract both the spectra
+    and key details of the observations.
+
+    Parameters
+    ----------
+    spectra_folder: string
+        Root directory of nightly extracted 1D fits cubes.
+
+    id_crossmatch_fn: default: 'data/mike_id_cross_match.tsv'
+
+    plot_folder: str, default: ''
+        Base folder to save diagnostic reduction plots to.
+
+    Returns
+    -------
+    TODO
+    """
+    # Import ID crossmatch file
+    id_cm_df = pd.read_csv(
+        filepath_or_buffer=id_crossmatch_fn,
+        sep="\t",
+        index_col="obj",
+        dtype={"source_id":str},)
+
+    # Get all fits filenames
+    spectra_wildcard = os.path.join(spectra_folder, "*.fits")
+    fits_files = glob.glob(spectra_wildcard)
+    fits_files.sort()
+    fits_files = np.array(fits_files)
+
+    n_spec = len(fits_files)
+
+    # Group fits files per star. Although the object name is in the filename,
+    # it's not reliably formatted, so the simple approach is to just grab the
+    # information straight from the fits headers instead.
+    obj_names = np.array([fits.getval(fn, "OBJECT") for fn in fits_files])
+    det_arms = np.array([fits.getval(fn, "INSTRUME") for fn in fits_files])
+    exp_times = np.array([fits.getval(fn, "EXPTIME") for fn in fits_files])
+    n_loops = np.array([fits.getval(fn, "NLOOPS") for fn in fits_files])
+    date_obs = np.array([fits.getval(fn, "DATE-OBS") for fn in fits_files])
+    ut_dates = np.array([fits.getval(fn, "UT-DATE") for fn in fits_files])
+
+    # Round exposure times
+    exp_times = np.round(exp_times, decimals=0).astype(int)
+
+    # Create a boolean mask for the blue or red detectors
+    is_red  = np.array(det_arms) == "MIKE-Red"
+
+    # Grab unique object names
+    unique_objs = set(obj_names)
+    
+    # Initialise objects to store all of this
+    blue_dict = {}
+    red_dict = {}
+
+    # Loop over all objects
+    for obj in unique_objs:
+        # Also loop over dates observed
+        ut_dates_obj = set(ut_dates[obj_names == obj])
+        for ut_date in ut_dates_obj:
+            print("\n{} ({})...".format(obj, ut_date))
+            # Read in blue data
+            is_obj_blue_arm_date = np.all(np.stack(
+                (obj_names == obj, ~is_red, ut_dates == ut_date)), axis=0)
+
+            if np.sum(is_obj_blue_arm_date) > 0:
+                obj_blue_fns = fits_files[is_obj_blue_arm_date]
+                print("\n\tBlue:\n\t", "\n\t".join(obj_blue_fns), sep="",)
+                exp_dict_b, obj_dict_b = load_and_combine_single_star(
+                    obj_blue_fns, plot_folder)
+            else:
+                exp_dict_b = None
+                obj_dict_b = None
+
+            # Read in red data
+            is_obj_red_arm_date = np.all(np.stack(
+                (obj_names == obj, is_red, ut_dates == ut_date)), axis=0)
+
+            if np.sum(is_obj_red_arm_date) > 0:
+                obj_red_fns = fits_files[is_obj_red_arm_date]
+                print("\n\t:\n\t", "\n\t".join(obj_red_fns), sep="",)
+                exp_dict_r, obj_dict_r = load_and_combine_single_star(
+                    obj_red_fns, plot_folder)
+            else:
+                exp_dict_r = None
+                obj_dict_r = None
+
+            # Combine
+            blue_dict[obj] = obj_dict_b
+            red_dict[obj] = obj_dict_r
+
+    return blue_dict, red_dict
+
+
+def visualise_mike_spectra(
+    data_dict,
+    plot_folder,
+    plot_fn,
+    plot_title,
+    title_colour,):
+    """Visualises extracted MIKE spectra, plotting panels for sky, star, sigma,
+    SNR, lamp, flat, and normalised star.
+
+    Parameters
+    ----------
+    data_dict: dict
+        Dictionary representation of MIKE fits file, with keys for relevant
+        header keywords, the wavelength scale, and each of the seven (sky, obj,
+        sigma, snr, lamp, flat, obj / flat) data-cubes.
+
+    plot_folder: str
+        Folder to save plots to.
+
+    plot_fn: str
+        Filename of the saved plot.
+
+    plot_title: str
+        Title of the plot.
+
+    title_colour: str
+        Colour of the title text.
+    """
+    waves = data_dict["wave"]
+    spec_sky = data_dict["spec_sky"]
+    spec_star = data_dict["spec_star"]
+    sigma_star = data_dict["sigma_star"]
+    snr_star = data_dict["snr_star"]
+    spec_lamp = data_dict["spec_lamp"]
+    spec_flat = data_dict["spec_flat"]
+    spec_star_norm = data_dict["spec_star_norm"]
+
+    plt.close("all")
+    fig, axes = plt.subplots(nrows=7, sharex=True, figsize=(16, 10))
+
+    fig.subplots_adjust(
+        left=0.075,
+        bottom=0.075,
+        right=0.98,
+        top=0.975,
+        hspace=0.000,
+        wspace=0.001)
+
+    axes[0].plot(waves.ravel(), spec_sky.ravel(), linewidth=0.2)
+    axes[1].plot(waves.ravel(), spec_star.ravel(), linewidth=0.2)
+    axes[2].plot(waves.ravel(), sigma_star.ravel(), linewidth=0.2)
+    axes[3].plot(waves.ravel(), snr_star.ravel(), linewidth=0.2)
+    axes[4].plot(waves.ravel(), spec_lamp.ravel(), linewidth=0.2)
+    axes[5].plot(waves.ravel(), spec_flat.ravel(), linewidth=0.2)
+    axes[6].plot(waves.ravel(), spec_star_norm.ravel(), linewidth=0.2)
+
+    labels = ["Sky", "Star", "Sigma", "SNR", "Lamp", "Flat", "Star (Norm)"]
+
+    for label_i, label in enumerate(labels):
+        axes[label_i].set_ylabel(label)
+
+    axes[-1].set_xlabel("Wavelength (Å)")
+    axes[0].set_title(plot_title, c=title_colour)
+
+    axes[-1].xaxis.set_major_locator(plticker.MultipleLocator(base=200))
+    axes[-1].xaxis.set_minor_locator(plticker.MultipleLocator(base=100))
+
+    # Check save folder and save
+    if not os.path.isdir(plot_folder):
+        os.mkdir(plot_folder)
+
+    plt.savefig(os.path.join(plot_folder, plot_fn))
