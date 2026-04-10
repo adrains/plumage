a71c1d22c1ecd5ddc1b7ac2242252629a972ec31
Adam D Rains <adam.d.rains@gmail.com>
2026-04-10 11:37:55 -0400

    Updates to binary crossmatching to e.g. exclude known binaries.

--- scripts_misc/crossmatch_el_badry_binaries.py
+++ scripts_misc/crossmatch_el_badry_binaries.py
@@ -4,30 +4,48 @@ import matplotlib.pyplot as plt
 from astropy.table import Table
 import pandas as pd
 import stannon.parameters as params
+import plumage.utils as pu
+import matplotlib.ticker as plticker
 
 lit_chem =  "data/lit_chemistry_corrected_Fe_Ti_Mg_Ca_Na.tsv"
 
-ff = fits.open("/Users/arains/Dropbox/code/plumage/data/El-Badry_all_columns_catalog.fits.gz")
+ff = fits.open("/Users/adamrains/Dropbox/code/plumage/data/El-Badry_all_columns_catalog.fits.gz")
 data = ff[1].data
+
+# -----------------------------------------------------------------------------
+# Import table
+# -----------------------------------------------------------------------------
+binary_tab = Table(data)
+binary_df = binary_tab.to_pandas()
+
+binary_df["source_id_dr3"] = binary_df["source_id1"].values.astype(str)
+binary_df.set_index("source_id_dr3", inplace=True)
+
+# Compute M_G
+dist2 = 1000 / binary_df["parallax2"].values
+binary_df["M_G_2"] = \
+    binary_df["phot_g_mean_mag2"].values - 5*np.log10(dist2/10)
 
 # -----------------------------------------------------------------------------
 # Masks
 # -----------------------------------------------------------------------------
 # BP-RP limits to ensure F/G/K primaries
-prim_mask = np.logical_and(data["bp_rp1"] > 1.2, data["bp_rp1"] < 2.5)
+prim_mask = data["bp_rp1"] <= 1.2
+#np.logical_and(data["bp_rp1"] > 1.2, data["bp_rp1"] < 2.5)
 
 # BP-RP limits to ensure K/M secondaries
-sec_mask = np.logical_and(data["bp_rp2"] > 3.5, data["bp_rp2"] < 5)
+sec_mask = np.logical_and(data["bp_rp2"] > 1.2, data["bp_rp2"] < 5)
 
 # Declination limit to target southern systems we don't already have
-dec_mask = np.logical_and(data["dec1"] < 25, data["dec1"] >= -90)
+dec_mask = np.logical_and(data["dec1"] < 90, data["dec1"] >= -90)
 
 # Parallax mask to target nearby stars
 plx_mask = data["parallax1"] > 1.0
 
 # Magnitude mask to ensure the stars are observable
-mag_mask = np.logical_and(
-    data["phot_rp_mean_mag1"] < 15, data["phot_rp_mean_mag2"] < 16)
+mag_mask = data["phot_rp_mean_mag1"] > 0
+#np.logical_and(
+#   data["phot_rp_mean_mag1"] < 15, data["phot_rp_mean_mag2"] < 16)
 
 # RUWE cut for primaries
 ruwe_mask_prim = data["ruwe1"] <= 1.4
@@ -35,16 +53,33 @@ ruwe_mask_prim = data["ruwe1"] <= 1.4
 # RUWE cut for secondaries
 ruwe_mask_sec = data["ruwe2"] <= 1.4
 
+# Secondary Parallax precision
+parallax_error = data["parallax_error2"] / data["parallax2"]
+unreliable_plx = parallax_error > 0.01
+
 # Separation cut to ensure they're widely enough separated to observe. This
 # column is in degrees, so we multiply by 3600 to get to arcseconds.
-sep_mask = data["pairdistance"]*3600 > 2
+sep_mask = data["pairdistance"]*3600 > 3
 
 # 'Probability' of chance alignment per Eqn 8 of paper.
 chance_align_mask = data["R_chance_align"] < 0.01
 
+# Exclude binaries that we already know about
+cpm_prim = pu.load_info_cat(
+    path="data/cpm_primaries_dr3.tsv",
+    make_observed_col_bool_on_yes=False,
+    use_mann_code_for_masses=False,
+    gdr="dr3",
+    has_2mass=False,)
+
 combined_mask = np.all((prim_mask, sec_mask, dec_mask, plx_mask, mag_mask,
                         ruwe_mask_prim, ruwe_mask_sec, sep_mask,
-                        chance_align_mask), axis=0)
+                        chance_align_mask, ~unreliable_plx), axis=0)
+
+binary_df = binary_df[combined_mask].copy()
+
+is_known_binary = np.array(
+    [sid in cpm_prim.index.values for sid in binary_df.index.values])
 
 # -----------------------------------------------------------------------------
 # Crossmatching with [Fe/H] and [X/Fe] catalogues
@@ -98,7 +133,8 @@ for star_i, (source_id, star_info) in enumerate(binary_df_join.iterrows()):
         mid_K_MKs_bound=mid_K_MKs_bound,
         abund_order_k=ABUND_ORDER_K,
         abund_order_m=None,
-        abund_order_binary=None,)
+        abund_order_binary=None,
+        mid_K_BP_RP_trustworthy_X_Fe=1.2,)
     feh_info_all.append(feh_info)
 
 feh_info_all = np.vstack(feh_info_all)
@@ -129,6 +165,121 @@ axes[1].set_xlabel("BP-RP")
 plt.tight_layout()
 axes[1].legend()
 """
+
+is_known_binary = np.array(
+    [sid in cpm_prim.index.values for sid in binary_df_join.index.values])
+
+plt.close("all")
+fig, axes = plt.subplots(ncols=5, figsize=(16,5))
+
+# Panel 1: [Fe/H] histogram
+axes[0].hist(
+    binary_df_join["Fe_H_adopt"].values,
+    bins=20,
+    label="All",
+    alpha=0.6)
+
+axes[0].hist(
+    binary_df_join["Fe_H_adopt"].values[~is_known_binary],
+    bins=20,
+    label="New",
+    alpha=0.6)
+
+axes[0].set_xlabel("[Fe/H]")
+axes[0].legend(fontsize="xx-small")
+
+# Panel 2: secondary BP-RP histogram
+axes[1].hist(
+    binary_df_join["bp_rp2"].values,
+    bins=15,
+    label="All ({})".format(is_known_binary.size),
+    alpha=0.6)
+
+axes[1].hist(
+    binary_df_join["bp_rp2"].values[~is_known_binary],
+    bins=15,
+    label="New ({})".format(np.sum(~is_known_binary)),
+    alpha=0.6)
+
+axes[1].set_xlabel("BP-RP")
+
+# Panel 3: secondary BP-RP vs [Fe/H]
+axes[2].plot(
+    binary_df_join["bp_rp2"].values,
+    binary_df_join["Fe_H_adopt"].values,
+    linestyle="",
+    marker="o",
+    ms="2",
+    #markeredgecolor="k",
+    label="All ({})".format(is_known_binary.size),)
+
+axes[2].plot(
+    binary_df_join["bp_rp2"].values[~is_known_binary],
+    binary_df_join["Fe_H_adopt"].values[~is_known_binary],
+    linestyle="",
+    marker="o",
+    ms="2",
+    #markeredgecolor="k",
+    label="New ({})".format(np.sum([~is_known_binary])),)
+
+axes[2].set_xlabel("BP-RP")
+axes[2].set_ylabel("[Fe/H]")
+
+# Panel 3: distances
+axes[3].hist(
+    binary_df_join["dist2"].values,
+    bins=15,
+    label="All ({})".format(is_known_binary.size),
+    alpha=0.6)
+
+axes[3].hist(
+    binary_df_join["dist2"].values[~is_known_binary],
+    bins=15,
+    label="New ({})".format(np.sum(~is_known_binary)),
+    alpha=0.6)
+
+axes[3].set_xlabel("Distance (pc)")
+
+# Panel 4: CMD
+scatter = axes[4].scatter(
+    binary_df_join["bp_rp2"].values,
+    binary_df_join["M_G_sec"].values, 
+    zorder=1,
+    c=binary_df_join["Fe_H_adopt"].values,
+    alpha=0.9,
+    label="All ({})".format(is_known_binary.size),
+    cmap="viridis",)
+
+_ = axes[4].scatter(
+    binary_df_join["bp_rp2"].values[is_known_binary],
+    binary_df_join["M_G_sec"].values[is_known_binary],
+    marker="o",
+    zorder=2,
+    edgecolor="black",
+    facecolors="none",
+    alpha=0.9,
+    label="Known ({})".format(np.sum(is_known_binary)),
+    cmap="viridis",)
+
+axes[4].legend(fontsize="xx-small")
+cb = fig.colorbar(scatter, ax=axes[4])
+cb.set_label("[Fe/H]")
+axes[4].set_xlabel(r"$BP-RP$", fontsize="large")
+axes[4].set_ylabel(r"$M_G$", fontsize="large")
+
+ymin, ymax = axes[4].get_ylim()
+axes[4].set_ylim((ymax, ymin))
+
+axes[1].xaxis.set_major_locator(plticker.MultipleLocator(base=0.5))
+axes[1].xaxis.set_minor_locator(plticker.MultipleLocator(base=0.25))
+axes[2].xaxis.set_major_locator(plticker.MultipleLocator(base=0.5))
+axes[2].xaxis.set_minor_locator(plticker.MultipleLocator(base=0.25))
+axes[4].xaxis.set_major_locator(plticker.MultipleLocator(base=0.5))
+axes[4].xaxis.set_minor_locator(plticker.MultipleLocator(base=0.25))
+
+
+plt.tight_layout()
+
 #------------------------------------------------------------------------------
 # Make CPM list for MIKE
 #------------------------------------------------------------------------------
